6f3f5e809cb063e8fb6777aec7813d19fc70877d
Aashish Maharjan <aashishmaharjan10@gmail.com>
2019-09-08 12:43:19 +0545

    User Created

---
+++ models/users.js
@@ -0,0 +1,26 @@
+var mongoose = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  firstname: {
+      type:String,
+      required: true
+  },
+  lastname: {
+      type: String,
+      required: true
+  },
+  email: {
+    type: String,
+    required: true
+},
+  admin: {
+    type: Boolean,
+    default: false
+  }
+});
+
+userSchema.plugin(passportLocalMongoose);
+var Users=mongoose.model('User',userSchema);
+module.exports = Users;
