79bf6fbd515a08dce4a4a0616fa7bb67946ab5a9
Aashish Maharjan <aashishmaharjan10@gmail.com>
2019-09-10 11:33:29 +0545

    College Models and Routers created

--- app.js
+++ app.js
@@ -3,11 +3,30 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+// var cors = require("cors");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var collegeRouter = require('./routes/college');
 
 var app = express();
+
+const url = "mongodb://localhost:27017/College";
+const connect = mongoose.connect(url, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+
+connect.then(
+  db => {
+    console.log("Connected to mongodb server");
+  },
+  err => {
+    console.log(err);
+  }
+);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +38,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// app.use('*', cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/college', collegeRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ models/college.js
@@ -0,0 +1,55 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var collegeSchema = new Schema({
+    college_name: {
+        type: String,
+        required: true,
+        default: false
+    },
+
+    location: {
+        type: String,
+        required: true
+    },
+
+    total_student: {
+        type: Number,
+        required: true
+    },
+
+    affiliation: {
+        type: String,
+        required: true
+    },
+
+    desc: {
+        type: String,
+        required: true
+    },  
+    
+    courses: {
+        type: String,
+        required: true
+    },
+
+    credit_hours: {
+        type: String,
+        required: true
+    },
+
+    Fees: {
+        type: Number,
+        required: true
+    },
+
+    Scholarship_criteria: {
+        type: String,
+        required: true
+    }
+
+});
+
+var Colleges = mongoose.model("College", collegeSchema);
+module.exports = Colleges;
+
